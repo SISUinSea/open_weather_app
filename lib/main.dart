988b43a0795e03e76fab01b43ba2d855d7e338da
SISU <atgsisu@gmail.com>
2022-08-29 17:06:00 +0900

    Add Changing Theme Feature based on Temperature
    2022.08.29 (7)
    
    77. ThemeState, ThemeProvider ChangeNotifier 및 Theme 변화 적용

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/providers/provider.dart';
 import 'package:weather_app/providers/temp_settings/temp_settings_provider.dart';
 import 'package:weather_app/providers/weather/weather_provider.dart';
 import 'package:weather_app/repositories/weather_repository.dart';
@@ -32,21 +33,18 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider<TempSettingsProvider>(
             create: (context) => TempSettingsProvider()),
+        ChangeNotifierProxyProvider<WeatherProvider, ThemeProvider>(
+          create: (context) => ThemeProvider(),
+          update: (BuildContext context, WeatherProvider wp,
+                  ThemeProvider? themeProvider) =>
+              themeProvider!..update(wp),
+        )
       ],
-      child: MaterialApp(
+      builder: (context, _) => MaterialApp(
         title: 'Weather app',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
+        theme: context.watch<ThemeProvider>().state.appTheme == AppTheme.light
+            ? ThemeData.light()
+            : ThemeData.dark(),
         home: const HomePage(),
       ),
     );
---
+++ lib/providers/theme/theme_provider.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/constants/constants.dart';
+
+import '../weather/weather_provider.dart';
+
+part 'theme_state.dart';
+
+class ThemeProvider with ChangeNotifier {
+  ThemeState _state = ThemeState.initial();
+  ThemeState get state => _state;
+
+  void update(WeatherProvider wp) {
+    if (wp.state.weather.temp > kWarmOrNot) {
+      _state = _state.copyWith(appTheme: AppTheme.light);
+      notifyListeners();
+    } else {
+      _state = _state.copyWith(appTheme: AppTheme.dark);
+      notifyListeners();
+    }
+  }
+}
